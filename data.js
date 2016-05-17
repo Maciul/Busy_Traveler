91b8328b6eee7bebf82df036b404404f577b44ea
Pawel <Pawel.Maciulewski@gmail.com>
2016-05-17 12:13:48 -0600

    Include general info display

--- data.js
+++ data.js
@@ -13,6 +13,7 @@ $('button').click(function(event) {
   specific = $('select.to option:selected').val()
   specific2 = $('select.from option:selected').val()
 
+
 // Getting values for calling different API's //
 $.when( $.ajax( 'https://restcountries.eu/rest/v1/alpha?codes='+specific+';'+specific2+'' ),
         $.ajax('https://knoema.com/api/1.0/meta/dataset/ICPR2011/dimension/region' ) ).done(function(data, region) {
@@ -34,16 +35,24 @@ $.when( $.ajax( 'https://restcountries.eu/rest/v1/alpha?codes='+specific+';'+spe
 
 // CURRENCY CALCULATOR - change if possible -----
   $.get('http://api.fixer.io/latest?base='+countryFrom.currency+'&symbols='+countryTo.currency+'', function(currency) {
-    console.log("Currency is "+currency.rates[countryTo.currency])
-    })
+      $('section:first-of-type +section').empty()
+    if (countryFrom.currency === countryTo.currency) {
+      $('section:first-of-type +section').append("Both countries have the same currency! No need to exchange!!!")
+    } else if (currency.rates[countryTo.currency] === undefined){
+      $('section:first-of-type +section').append('Sorry, no information on that exchange rate check out:')
+    }else {
+      $('section:first-of-type +section').append('1 '+countryFrom.currency+' gets you a whooping ' +currency.rates[countryTo.currency]+' '+countryTo.currency)
+    }
+  })
 // FINANCIAL Purchasing power parity stats -- how to display??
   $.get('https://knoema.com/api/1.0/data/ICPR2011?Time=2011-2011&region='
   +countryFrom.id+','+countryTo.id+'&measures-components=1000270,1000260,1000360&economic-aggregates=1000190&Frequencies=A', function(financial) {
 
     PPP = financial.data //Purchasing Power Parity //
-    var alcohol =100 -  Math.round(((PPP[0].Value / PPP[3].Value) * 100));
-    var food = 100 - Math.round(((PPP[1].Value / PPP[4].Value) * 100));
-    var restaurant =100 - Math.round(((PPP[2].Value / PPP[5].Value) * 100));
+
+    var alcohol =100 -  Math.round(((PPP[0].Value - PPP[3].Value) / PPP[0].Value) *100)-100;
+    var food = 100 - Math.round(((PPP[1].Value - PPP[4].Value) / PPP[1].Value * 100)-100);
+    var restaurant = 100 - Math.round(((PPP[2].Value - PPP[5].Value) / PPP[2].Value * 100)-100);
 
     console.log("Alcohol will be " +alcohol+ " Food will be " +food+ " Hotels " +restaurant)
   })
@@ -51,11 +60,16 @@ $.when( $.ajax( 'https://restcountries.eu/rest/v1/alpha?codes='+specific+';'+spe
   $.get('https://restcountries.eu/rest/v1/alpha/'+countryTo.A2, function(general) {
 
     var lastElement = general.altSpellings.length - 1;
-    console.log(general.name)
-    console.log(general.altSpellings[lastElement])
-    console.log(general.capital)
-    console.log(general.subregion)
+    console.log(general)
+    $('section:first-of-type').empty()
+    $('section:first-of-type').append('<p>Country: '+general.name+'<p>')
+    $('section:first-of-type').append('<p> Official Name: '+general.altSpellings[lastElement]+'<p>')
+    $('section:first-of-type').append('<p>Capital: '+general.capital+'<p>')
+    $('section:first-of-type').append('<p>Region: '+general.subregion+'<p>')
+    $('section:first-of-type').append('<p>Currency: '+general.currencies[0]+'<p>')
     })
+
+
 // Call for SAFETY information
   var tuGroup = {
   "url": "https://api.tugroup.com/v1/travelsafe/countries/"+countryTo.A2,
@@ -77,7 +91,6 @@ $.ajax(tuGroup).done(function (safety) {
 
     })
   })
-
 })
 })
 })
